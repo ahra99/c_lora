170807eb3d040f797a9b13c8f4f987aaff96c28d
ahra99 <ah.rahmati.j@gmail.com>
2025-10-12 20:37:01 -0500

    v1.0

--- models/causallm.py
+++ models/causallm.py
@@ -21,45 +21,10 @@ class CausalLM(nn.Module):
         if args.load_checkpoint:
             print('=====================')
             if args.ood_ori_dataset is not None:
-                if ('light' in args.modelwrapper) and (args.bayes_eval_n_samples_final == 0) : 
-                    if args.seed == 3:# or args.seed == 6 or args.seed == 9:#1: # since I did some weird way of saving the weights for obqa seed 1 ...... Feb 19 ===> modified on Apr 22 for debugging
-                        # args.load_path = f'/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft/last_models/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/{args.checkpoint_dic_name}/3'#/adam'#{args.load_model_path}'
-                        args.load_path = f'/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft/bstm_obqa/5/{args.bayes_eval_n_samples_final}/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/{args.checkpoint_dic_name}/{args.seed}'#/adam'#{args.load_model_path}'
-                        # args.load_path = f'/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft/bstm/0/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/{args.checkpoint_dic_name}/{args.seed}'#/adam'#{args.load_model_path}'
-                        # args.load_path = f'/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft/bests/5/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/{args.checkpoint_dic_name}/{args.seed}'#/adam'#{args.load_model_path}'
-                        # args.load_path = f'/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft/save_best_model_check1/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/{args.checkpoint_dic_name}/{args.seed}'#/adam'#{args.load_model_path}'
-                        # args.load_path = f'/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft/checkpoints/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/default/adam.pt/'#/adam'#{args.load_model_path}'
-                    elif args.seed == 6:
-                        args.load_path = f'/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft/bstm_obqa/8/{args.bayes_eval_n_samples_final}/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/{args.checkpoint_dic_name}/{args.seed}'#/adam'#{args.load_model_path}'
-
-                    elif args.seed == 9:
-                        args.load_path = f'/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft/bstm_obqa/3/{args.bayes_eval_n_samples_final}/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/{args.checkpoint_dic_name}/{args.seed}'#/adam'#{args.load_model_path}'
-
-                if ('light' in args.modelwrapper) and (args.bayes_eval_n_samples_final == 10) : 
-                    if args.seed == 3:# or args.seed == 6 or args.seed == 9:#1: # since I did some weird way of saving the weights for obqa seed 1 ...... Feb 19 ===> modified on Apr 22 for debugging
-                        # args.load_path = f'/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft/last_models/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/{args.checkpoint_dic_name}/3'#/adam'#{args.load_model_path}'
-                        args.load_path = f'/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft/bstm_obqa/2/{args.bayes_eval_n_samples_final}/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/{args.checkpoint_dic_name}/{args.seed}'#/adam'#{args.load_model_path}'
-                        # args.load_path = f'/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft/bstm/0/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/{args.checkpoint_dic_name}/{args.seed}'#/adam'#{args.load_model_path}'
-                        # args.load_path = f'/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft/bests/5/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/{args.checkpoint_dic_name}/{args.seed}'#/adam'#{args.load_model_path}'
-                        # args.load_path = f'/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft/save_best_model_check1/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/{args.checkpoint_dic_name}/{args.seed}'#/adam'#{args.load_model_path}'
-                        # args.load_path = f'/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft/checkpoints/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/default/adam.pt/'#/adam'#{args.load_model_path}'
-                    elif args.seed == 6:
-                        args.load_path = f'/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft/bstm_obqa/6/{args.bayes_eval_n_samples_final}/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/{args.checkpoint_dic_name}/{args.seed}'#/adam'#{args.load_model_path}'
-
-                    elif args.seed == 9:
-                        args.load_path = f'/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft/bstm_obqa/1/{args.bayes_eval_n_samples_final}/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/{args.checkpoint_dic_name}/{args.seed}'#/adam'#{args.load_model_path}'
-
-
-                elif args.modelwrapper == 'blob':
-                    args.load_path = f'/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft/last_models/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/{args.checkpoint_dic_name}/{args.seed}'
-
-                elif ('light' in args.modelwrapper) and (args.bayes_eval_n_samples_final < 0):
-                    args.load_path = f'/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft/last_models/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/{args.checkpoint_dic_name}/{args.seed}'
-
-                else: 
-                    # /teamspace/studios/this_studio/bayesian-peft/last_models/mle/meta-llama/Llama-2-7b-hf/obqa/default/3/
-                    args.load_path = f'/teamspace/studios/this_studio/bayesian-peft/last_models_1/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/{args.checkpoint_dic_name}/{args.seed}'#/adam'#{args.load_model_path}'
-                    # args.load_path = f'/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft/checkpoints/{args.modelwrapper}/{args.model}/{args.ood_ori_dataset}/{args.checkpoint_dic_name}/{args.seed}'#/adam'#{args.load_model_path}'
+                if ('c_lora' in args.modelwrapper) and (args.bayes_eval_n_samples_final == 0) : 
+                    # for each seed, load the best model
+                    print("load the best saved model")   
+                
             else:
                 args.load_path = f'checkpoints/{args.modelwrapper}/{args.model}/{args.dataset}/{args.load_model_path}'
             print('Loading model from: ', args.load_path)
@@ -69,7 +34,7 @@ class CausalLM(nn.Module):
             )
             # /teamspace/studios/this_studio/bayesian-peft/last_models/
             model = AutoModelForCausalLM.from_pretrained(peft_config.base_model_name_or_path, quantization_config=bnb_config, cache_dir='/teamspace/studios/this_studio/bayesian-peft/')
-            # model = AutoModelForCausalLM.from_pretrained(peft_config.base_model_name_or_path, quantization_config=bnb_config, cache_dir='/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft')
+            
             self.model = PeftModel.from_pretrained(model, args.load_path, is_trainable=True)
             modelwrapper = get_modelwrapper(args.modelwrapper)
             self.model = modelwrapper(self.model, peft_config, args, accelerator, adapter_name="default")
@@ -82,9 +47,9 @@ class CausalLM(nn.Module):
                 load_in_8bit=args.load_in_8bit
             )
             if args.load_model_path is not None:
-                model = AutoModelForCausalLM.from_pretrained(args.load_model_path, quantization_config=bnb_config)#, cache_dir='/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft')
+                model = AutoModelForCausalLM.from_pretrained(args.load_model_path, quantization_config=bnb_config)
             else:
-                model = AutoModelForCausalLM.from_pretrained(args.model, quantization_config=bnb_config)#, cache_dir='/scratch/user/amir_hossein_rahmati/BayesPEFT/bayesian-peft')
+                model = AutoModelForCausalLM.from_pretrained(args.model, quantization_config=bnb_config)
             if args.apply_classhead_lora:
                 target_modules=["q_proj", "v_proj", "lm_head"]
             elif args.apply_qkv_head_lora:
